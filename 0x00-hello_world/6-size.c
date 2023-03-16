1637ae28babb3af7ed78285f74cbd5fe7dc1b28d
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-16 20:26:51 +0000

    Size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %ul byte(s)", (unsigned long)sizeof(a));
-printf("Size of an int: %ul byte(s)", (unsigned long)sizeof(b));
-printf("Size of a long int: %ul byte(s)", (unsigned long)sizeof(c));
-printf("Size of a long long int: %ul byte(s)", (unsigned long)sizeof(d));
-printf("Size of a float: %ul byte(s)", (unsigned long)sizeof(e));
+printf("Size of a char: %ul byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %ul byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %ul byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %ul byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %ul byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }

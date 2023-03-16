dcad1ba655d8bccb892ca51e565d9ec971147122
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-16 17:13:31 +0000

    Printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main() - prints a string using the printf() function
+*@void: Indicates no parameter
+*
+*Return: Returns 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+}
+

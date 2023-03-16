3b9f577b06d374f5e2ac8b75849f9a7318a650e9
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-16 21:17:57 +0000

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 */
 int main(void)
 {
-fwrite(STDOUT_FILENO, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+fwrite(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 30);
 return (1);
 }

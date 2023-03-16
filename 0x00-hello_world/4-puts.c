b954af65d674ef7af460dae4a40f2bc380087aa8
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-16 16:41:05 +0000

    Hello, puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+* main - prints out a string using puts()
+* @void: Indicates no parameter
+
+* Return: The return value is 0
+*/
 int main(void)
 {
-  char my_string[] = "Programming is like buliding a multilingual puzzle\n";
-  puts(my_string);
-  return 0;
-
+char my_string[] = "\"Programming is like buliding a multilingual puzzle\n";
+puts(my_string);
+return (0);
 }

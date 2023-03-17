27c6094a4b165cfc3d54a4afe8846bed0a9b6d25
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-17 23:33:30 +0000

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main -Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int num, alph;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (alph = 'A'; alph <= 'F'; alph++)
+		putchar(alph);
+	putchar('\n');
+	return (0);
+}

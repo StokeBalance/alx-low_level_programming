0d9e31128849eb89645c04b6ef709a97f9b95404
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-17 23:00:48 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Result: Always 0
+*/
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');	
+	return (0);
+}

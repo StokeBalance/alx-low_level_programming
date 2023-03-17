31aae97445c3a5d54d463861899d54eea9b93769
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-17 22:02:01 +0000

    Write alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+*Return: Alawys 0
+*/
+int main(void)
+{
+	char alph = 'a';
+	while (alph <= 'z')
+		putchar(alph);
+		putchar('\n');
+		alph++;
+	return (0);
+
+}

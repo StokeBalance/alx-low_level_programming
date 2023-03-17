42caa6748087fbb5326b9db37023a2d3ca1e8e55
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-17 22:31:24 +0000

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph, Alph;
+
+	for (alph == 'a'; alph <= 'z'; alph++)
+			putchar(alph);
+	for (Alph == 'A'; Alph <= 'Z'; Alph++)
+		putchar(Alph);
+	putchar('\n');
+	return (0);
+}

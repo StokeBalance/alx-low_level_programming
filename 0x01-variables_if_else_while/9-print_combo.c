a704cea509226c9e53699af577cb7254c8da67ad
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-18 00:03:13 +0000

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int nums;
+
+	for (nums = '0'; nums <= '9'; nums++)
+	{
+		putchar(nums);
+		if (nums != '9')
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

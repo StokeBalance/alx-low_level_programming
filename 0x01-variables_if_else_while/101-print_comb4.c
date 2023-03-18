1785ae27f4dfb137be32606d74d7d00934ad22e9
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-18 01:05:44 +0000

    Triple digits combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+	int f, m, l;
+
+	for (f = '0'; f < '8'; f++)
+	{
+		for (m = f + 1; m < '9'; m++)
+		{
+			for (l = m + 1; l <= '9'; l++)
+			{
+				if (( f != m) != l)
+				{
+					putchar(f);
+					putchar(m);
+					putchar(l);
+
+					if ( f == 7 && m == 8 && l == 9)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

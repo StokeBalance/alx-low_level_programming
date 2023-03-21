17b7579bcc974494ea2e8b6c3816a8c5a4d70977
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-21 23:37:54 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/* print_last_digit - prints last digit of a number
+/**
+* print_last_digit - prints last digit of a number
 * @r: parameter to be checked
 *
 * Return: 0
@@ -12,6 +13,8 @@ int print_last_digit(int r)
 	if (r < 0)
 		r = -r;
 	a = r % 10;
+	if (a < 0)
+		a = -a;
 	_putchar(a + '0');
 	return (a);
 }

647464e5b6d2851755b6c88eab385b737491e928
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-21 22:01:04 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_alphabet - ptints all alphabets
+*/
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}

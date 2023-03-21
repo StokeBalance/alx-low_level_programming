366fa76f6c2305fbb00d841859a08d5b4d1768c4
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-21 22:54:40 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - prints absolute value of numbers
+* @n: the number to check
+*
+* Return: absolute value of n
+*/
+
+int _abs(int n)
+{
+	if (n < 0 || n >= 0)
+		return n;
+}

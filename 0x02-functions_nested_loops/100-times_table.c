e267dd6768416397b510886701de8de62b4ca889
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-22 02:05:04 +0000

    times table to 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -20,6 +20,7 @@ void print_times_table(int n)
 				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');

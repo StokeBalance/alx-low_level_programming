869248b69873dfc175d449f11dc8bfdbd7181128
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-23 18:07:04 +0000

    No infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -11,8 +12,8 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	/*{
+	/*while (i < 10)
+	{
 		putchar(i);
 	}*/
 

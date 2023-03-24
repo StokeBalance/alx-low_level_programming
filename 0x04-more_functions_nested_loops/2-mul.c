61358b54176fb0480d2c8c61e5937af222678585
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-24 11:19:29 +0000

    ismul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - multiplies two integers.
+*@a: An input integer
+*@b: An input integer
+* Return: Always 0.
+*/
+int main(int a, int b)
+{
+	return (a *b);
+}

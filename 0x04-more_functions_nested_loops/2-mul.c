ec899eab31ac3d263e6bcc65f4e882ff58dedd2d
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-24 11:25:47 +0000

    ismul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
-* mul - multiplies two integers.
-*@a: An input integer
-*@b: An input integer
-* Return: a * b
+* mul - multiplies two integers
+* @a: An input integer
+* @b: An input integer
+* Return: Multiple of a and b
 */
+
 int mul(int a, int b)
 {
 	return (a *b);

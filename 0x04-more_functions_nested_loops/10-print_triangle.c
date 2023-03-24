91023a8e89ff24c18568ee9e6b7d5806c055f6ce
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-24 14:53:58 +0000

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,23 @@
 
 void print_triangle(int size)
 {
-	int row, hashes, spaces;
+	int i = 0, j, n = size - 1;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
-		for (row = 0; row < size; row++)
+		for (; i < size; i++)
 		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-				_putchar(' ');
-			for (hashes = 0; hashes < row; hashes++)
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+			else
 				_putchar('#');
+			}
+			n--;
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }

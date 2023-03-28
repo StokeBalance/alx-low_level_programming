23422525394e7e11638d2920405264e794caab54
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-28 22:43:38 +0000

    Prints half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,19 @@
 
 void puts_half(char *str)
 {
-	int i, len = 0;
-	int n = (len + 1) / 2;
+	int i, n, len = 0;
 
 	while (str[len] != '\0')
 		len++;
 
 	if (len % 2 == 0)
-	{
-		for (i = (len / 2); i < len; i++)
-			_putchar(str[i]);
-	}
+		n = len / 2;
+
 	else if (len % 2 != 0)
-	{
-		for (i = n; i < len; i++)
-			_putchar(str[i]);
-	}
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }

45183be36d3332a7d6fd566d4cb66d02f0f96ad8
Ademola <ademolaakinsemoyin@gmail.com>
2023-03-28 22:58:53 +0000

    Print array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_array - Prints number of elements in an array
+* @a: Parameter that is a pointer to an integer
+* @n: Parameter that is an integer
+*
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+
+	_putchar('\n');
+}

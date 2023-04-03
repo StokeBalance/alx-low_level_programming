d77e6057004feb552f426493469aa82a6ad8dfb6
Ademola <ademolaakinsemoyin@gmail.com>
2023-04-03 19:07:45 +0000

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_chessboard - a function that prints a chessboard
+* @a: an array input to print
+* Return: Nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+		int i = 0, j = 0;
+
+		while (i < 8)
+		{
+			while (j < 8)
+			{
+				_putchar(a[i][j]);
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+}

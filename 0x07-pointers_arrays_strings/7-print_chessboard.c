1ed2f1b7282a4c4fe5a74edc06b4ef889720457a
Ademola <ademolaakinsemoyin@gmail.com>
2023-04-03 19:27:42 +0000

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,14 +10,11 @@ void print_chessboard(char (*a)[8])
 {
 		int i = 0, j = 0;
 
-		while (i < 8)
+		for (i = 0, i < 8, i++)
 		{
-			while (j < 8)
-			{
+			for (j = 0, j < 8, j++)
 				_putchar(a[i][j]);
-				j++;
-			}
+
 			_putchar('\n');
-			i++;
 		}
 }
